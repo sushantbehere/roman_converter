a7a207c50a1e3fc6047fe2faeb94cadddcf42488
JamesDracupWCU <108743432+JamesDracupWCU@users.noreply.github.com>
2026-03-02 17:23:43 -0500

    Combined tests

--- tests/roman_converter/dracup/test_to_roman.py
+++ tests/roman_converter/dracup/test_to_roman.py
@@ -28,4 +28,28 @@ def test_to_roman(impl: ModuleType):
     assert impl.to_roman(1994) == "MCMXCIV"  
     assert impl.to_roman(2421) == "MMCDXXI"  
 
+def test_from_roman(impl: ModuleType):
+    # Minimal and maximal
+    assert impl.from_roman("I") == 1
+    assert impl.from_roman("MMMCMXCIX") == 3999
 
+    # Basic additive numbers
+    assert impl.from_roman("II") == 2
+    assert impl.from_roman("III") == 3
+    assert impl.from_roman("VI") == 6
+    assert impl.from_roman("VIII") == 8
+    assert impl.from_roman("XI") == 11
+    assert impl.from_roman("XX") == 20
+
+    # Subtractive notation
+    assert impl.from_roman("IV") == 4
+    assert impl.from_roman("IX") == 9
+    assert impl.from_roman("XL") == 40
+    assert impl.from_roman("XC") == 90
+    assert impl.from_roman("CD") == 400
+    assert impl.from_roman("CM") == 900
+
+    # Mixed numbers
+    assert impl.from_roman("LVIII") == 58
+    assert impl.from_roman("MCMXCIV") == 1994
+    assert impl.from_roman("MMCDXXI") == 2421
