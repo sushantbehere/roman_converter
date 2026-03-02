7f6a82872d688d9c39513f4ff7fd827eaed2e56a
renross <briross@udel.edu>
2026-03-01 20:44:33 -0500

    from_roman implementations

--- src/roman_converter/ross_c.py
+++ src/roman_converter/ross_c.py
@@ -10,9 +10,20 @@ def to_roman(arabic: int) -> str:
     return conversion
 
 def from_roman(roman: str) -> int:
-    arabic_map: list[tuple[str, int]] = [
-        ("I", 1), ("IV", 4), ("V", 5), ("IX", 9), ("X", 10),
-        ("XL", 40), ("L", 50), ("XC", 90), ("C", 100),
-        ("CD", 400), ("D", 500), ("CM", 900), ("M", 1000)
-    ]
-    return 0
+    arabic_map: dict[str, int] = {
+        "I": 1, "V": 5, "X": 10,
+        "L": 50, "C": 100,
+        "D": 500, "M": 1000
+    }
+    i:int = 0
+    conversion:int = 0
+
+    while i < len(roman):
+        if i + 1 < len(roman) and arabic_map[roman[i]] < arabic_map[roman[i + 1]]:
+            conversion += arabic_map[roman[i + 1]] - arabic_map[roman[i]]
+            i += 2
+        else:
+            conversion += arabic_map[roman[i]]
+            i += 1
+
+    return conversion
